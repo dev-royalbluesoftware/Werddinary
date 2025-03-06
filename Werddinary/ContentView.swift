ec39ca26f935439afd158a585983b92b87ebb208
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-03-06 12:59:40 -0700

    Werddinary initial commit

---
+++ Werddinary/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//
+// Werddinary
+// ContentView.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Werddinary/WerddinaryApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// Werddinary
+// WerddinaryApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct WerddinaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
